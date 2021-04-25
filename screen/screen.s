438657423fec6ec2616bd95f648169d95d32c514
Martin Vilcans <martin@librador.com>
2021-04-25 19:40:40 +0200

    Save, restore, and invert in screen module

--- screen/screen.s
+++ screen/screen.s
@@ -1,2 +1,69 @@
+    EXTERN invert_screen
+    EXTERN save_screen_attributes
+    EXTERN restore_screen_attributes
+
+    SECTION lowmem
+invert_screen:
+    ld hl,$4000
+    ld d,$ff
+.each_pix:
+    ld a,(hl)
+    xor d
+    ld (hl),a
+    inc hl
+    ld a,h
+    cp $58
+    jr nz,.each_pix
+
+.each_attr:
+    ld a,(hl)
+    ld d,a
+
+    ; Paper to ink
+    rra
+    rra
+    rra
+    and 7
+    ld e,a
+
+    ; ink to paper
+    ld a,d
+    rla
+    rla
+    rla
+    and 70o
+    or e
+    ld e,a
+
+    ld a,d
+    and $c0  ; keep flash and bright
+    or e
+
+    ld (hl),a
+    inc hl
+    ld a,h
+    cp $5b
+    jr nz,.each_attr
+
+    ret
+
+save_screen_attributes:
+    ld hl,$5800
+    ld de,saved_attributes
+    ld bc,$300
+    ldir
+    ret
+
+restore_screen_attributes:
+    ld hl,saved_attributes
+    ld de,$5800
+    ld bc,$300
+    ldir
+    ret
+
     SECTION screen
     INCBIN "screen.scr"
+
+    SECTION .bss
+saved_attributes:
+    ds $300
--- screen/test_screen.s
+++ screen/test_screen.s
@@ -1,6 +1,54 @@
     SECTION .text
+
+    ld hl,$4000
+    ld de,save_screen
+    ld bc,$1b00
+    ldir
+
 p:
-    out ($fe),a
-    inc a
-    and 7
+    ld hl,save_screen
+    ld de,$4000
+    ld bc,$1b00
+    ldir
+    ld a,1     ; original
+    call wait
+
+    call save_screen_attributes
+
+    ld hl,$5800
+    ld de,$5801
+    ld bc,$2ff
+    ld (hl),17o
+    ldir
+    ld a,6    ; no attributes
+    call wait
+
+    call restore_screen_attributes
+
+    call invert_screen
+    ld a,2  ;inverted
+    call wait
+
+    ld hl,$5800
+    ld de,$5801
+    ld bc,$2ff
+    ld (hl),17o
+    ldir
+    ld a,5    ; no attributes
+    call wait
+
     jr p
+
+wait:
+    out ($fe),a
+    ld b,100
+.w:
+    push bc
+    call wait_frame
+    pop bc
+    djnz .w
+    ret
+
+    SECTION .bss
+save_screen:
+    ds $1b00

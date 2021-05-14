8fb1d7c38ccb1df6d9fd73eb94e9e70be593e7bd
Martin Vilcans <martin@librador.com>
2021-05-15 01:00:20 +0200

    Use that BASIC USR command sets BC

--- loader/loader.s
+++ loader/loader.s
@@ -1,25 +1,18 @@
     SECTION .text
 
+reference_point:
 	di
 	ld sp,0
 
 	; This code is relocatable,
-	; so we need to figure out where in memory we are
-	; Put a $c9 (ret instruction at $ffff) and call it.
-	ld a,$c9
-	push af
-	call $ffff
-reference_point:
-	dec sp
-	dec sp
-	pop hl
-	; hl now points to reference_point
-
+	; so we need to figure out where in memory we are.
+	; Using the fact that BASIC set BC to the argument to USR,
+	; so BC = reference_point.
 	; Copy compressed data and decompress function
 	; so that decompress appears at decompress_address
 	; Copy backwards so source and destination may overlap
 
-	ld bc,copy_end-reference_point-1
+	ld hl,copy_end-reference_point-1
 	add hl,bc
 	ld de,decompress_address+decompress_size-1
 	ld bc,copy_end-copy_start

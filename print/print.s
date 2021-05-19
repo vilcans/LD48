ef4ebf3725cdd0e8659f2fa28aa2449c4a57c27d
Martin Vilcans <martin@librador.com>
2021-05-19 22:00:39 +0200

    Print with spacing

--- print/print.s
+++ print/print.s
@@ -1,10 +1,33 @@
     EXTERN print
+    EXTERN print_with_spacing
 
 font = $3d00 - 32 * 8
 
     SECTION .text
+
 print:
-.each_char:
+; Print with default spacing (7)
+; In:
+;   HL = text string, null terminated
+;   DE = screen address
+; Out:
+;   HL = byte after null terminator
+    ld a,7
+    ;fallthrough
+print_with_spacing:
+; Print with specific spacing
+; In:
+;   A = pixels between characters
+;   HL = text string, null terminated
+;   DE = screen address
+; Out:
+;   HL = byte after null terminator
+    ld (spacing),a
+    ld ix,shift_jumps
+    xor a   ; initial shift
+    jp into_loop
+
+each_char:
     ld a,(hl)  ; character
     inc hl
     or a
@@ -23,14 +46,88 @@ print:
 
     push de
     ld b,8
-.each_row:
-    ld a,(hl)
+each_row:
+    push bc
+
+    ld a,(hl)   ; char bitmap
     inc l
+    ld c,0
+
+jr_amount = $+1
+    jr jr_start
+jr_start:
+shift0:
+    ex de,hl
+    or (hl)
+    ld (hl),a
+    ex de,hl
+    jp next_row
+shift7:
+    sra a
+    rr c
+shift6:
+    sra a
+    rr c
+shift5:
+    sra a
+    rr c
+shift4:
+    sra a
+    rr c
+shift3:
+    sra a
+    rr c
+shift2:
+    sra a
+    rr c
+shift1:
+    sra a
+    rr c
+
+    ld b,a
+    ld a,(de)
+    or b
     ld (de),a
+    inc e
+    ld a,(de)
+    or c
+    ld (de),a
+    dec e
+
+next_row:
     inc d
-    djnz .each_row
+
+    pop bc
+    djnz each_row
 
     pop de
-    inc e
     pop hl
-    jp .each_char
+
+    ld a,(shift)
+spacing = $+1
+    add 0
+.compare:
+    cp 8
+    jp c,.not_next_byte
+    sub 7
+    inc e
+    jr .compare
+.not_next_byte:
+into_loop:
+    ld (shift),a
+
+shift = $+2
+    ld a,(ix+0)
+    ld (jr_amount),a
+
+    jp each_char
+
+shift_jumps:
+    db shift0-jr_start
+    db shift1-jr_start
+    db shift2-jr_start
+    db shift3-jr_start
+    db shift4-jr_start
+    db shift5-jr_start
+    db shift6-jr_start
+    db shift7-jr_start
--- print/test_print.s
+++ print/test_print.s
@@ -1,17 +1,35 @@
-    ld ($1000),a
     ld hl,$5800
     ld de,$5801
     ld (hl),70o
     ld bc,$2ff
     ldir
 
+    ld hl,$3c00+'H'*8
+    ld de,$4020
+    ld b,8
+.lp:
+    ld a,(hl)
+    inc hl
+    ld (de),a
+    inc d
+    djnz .lp
+
     ld hl,text
     ld de,$4000
+    ld ($1000),a
     call print
+
+    ld hl,text2
+    ld de,$4040
+    ld a,9
+    call print_with_spacing
+
 .e:
     ld hl,$581f
     inc (hl)
     jr .e
 
 text:
-    db 'HELLO WORLD!',0
+    db 'HELLO WORLD! Welcome to my lair!',0
+text2:
+    db 'hello world!',0
